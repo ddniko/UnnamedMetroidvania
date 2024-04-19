e9d1fac9bb7a0b6a52557a39077fc86c4e00ecf3
Simpelmee <161465386+Simpelmee@users.noreply.github.com>
2024-04-19 10:34:33 +0200

    Should work

--- UnnamedMetroidvania/Assets/Scripts/Player Scripts/Movement/PlayerMovementDash.cs
+++ UnnamedMetroidvania/Assets/Scripts/Player Scripts/Movement/PlayerMovementDash.cs
@@ -6,6 +6,7 @@ using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 using UnityEngine.SocialPlatforms;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class NewPlayerMovement : MonoBehaviour
 {
@@ -520,6 +521,9 @@ public class NewPlayerMovement : MonoBehaviour
             Debug.Log("I'm too young to die...");
             RB.transform.position = respawnPoint;
             PHP = MaxPHP;
+            GameObject.Find("definitely boss").SendMessage("DespawnBoss");
+            GameObject.Find("LockedPositionCamera").GetComponent<CinemachineVirtualCamera>().enabled = false;
+            GameObject.Find("Virtual Camera").GetComponent<CinemachineVirtualCamera>().enabled = true;
         }
         #endregion
     }
